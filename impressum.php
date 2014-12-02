27190efdb6da438e5711c527d8fa4861d626ad8d
Michael Janneck <michael@janneck.de>
2014-12-02 16:43:08 +0100

    Impressum eingepflegt.

---
+++ impressum.php
@@ -0,0 +1,20 @@
+<h2>Impressum</h2>
+<h3>Anbieter und inhaltlich Verantwortlicher</h3>
+<p>Thomas-Mann-Schule L&uuml;beck<br>
+Thomas-Mann-Stra&szlig;e 14<br>
+23564 L&uuml;beck</p>
+
+<p>Schulleiter: Herr Peter Flittiger</p>
+
+<p>E-Mail: Thomas-Mann-Schule.Luebeck AT schule.landsh.de<br>
+Telefon: 0451 1228614</p>
+
+<h3>Urheberrecht</h3>
+<p>Soweit nicht explizit anders angegeben, d&uuml;rfen alle Inhalte nur mit ausdr&uuml;cklichen Zustimmung durch die Schulleitung der Thomas-Mann-Schule verwendet werden.</p>
+<p>Sollten Sie Grund zu der Annahme haben, dass auf diesen Seiten Material ver&ouml;ffentlicht wird, durch das Ihre Rechte oder die Rechte Dritter verletzt werden, dann informieren Sie uns bitte. Wir werden es ggf. umgehend entfernen.</p>
+
+<h3>Haftung f&uuml;r eigene Inhalte</h3>
+<p>Wir &uuml;bernehmen keinerlei Gew&auml;hr f&uuml;r die Aktualit&auml;t, Korrektheit, Vollst&auml;ndigkeit oder Qualit&auml;t der bereitgestellten Informationen und k&ouml;nnen daher auch nicht f&uuml;r Sch&auml;den haftbar gemacht werden, die durch die Nutzung der angebotenen Informationen verursacht wurden. Wir behalten uns vor, jederzeit &Auml;nderungen oder Erg&auml;nzungen vorzunehmen.</p>
+
+<h3>Verlinkung zu fremden Inhalten</h3>
+<p>Trotz sorgf&auml;ltiger inhaltlicher Kontrolle &uuml;bernehmen wir keine Haftung f&uuml;r die Inhalte von Links, die nicht auf die Domain tms-hl.de verweisen. F&uuml;r den Inhalt der verlinkten Seiten sind ausschlie&szlig;lich deren BetreiberInnen verantwortlich.</p>
